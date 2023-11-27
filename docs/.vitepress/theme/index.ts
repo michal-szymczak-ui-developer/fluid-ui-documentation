9e07f33213831b9dbadd5377b8c724861804b51d
Michal Szymczak <kontakt@michalszymczak.com>
2023-11-27 12:18:31 +0100

    Adding AI Chat

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,12 +2,18 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import Documate from "@documate/vue";
+import "@documate/vue/dist/style.css";
 import "./style.scss";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+      "nav-bar-content-before": () =>
+        h(Documate, {
+          endpoint: "https://z5tbqsc6t9.us.aircode.run/ask",
+        }),
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
